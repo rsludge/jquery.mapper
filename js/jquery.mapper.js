3b97e947437ab9c3adc75856f0ba68fad0735907
rsludge <devourerO@gmail.ru>
2011-09-18 01:29:36 +0400

    fill and stroke style

--- js/jquery.mapper.js
+++ js/jquery.mapper.js
@@ -2,9 +2,13 @@
     $.fn.mapper = function(userOptions){
 
         var options = {
+            strokeColor: '#0000ff',
+            strokeOpacity: 1.0,
+            strokeWidth: 3,
+            fillColor: '#ff0000',
+            fillOpacity: 0.5
         }
-        
-  
+
         return this.each(function(){
             var $image = $(this);
 
@@ -34,14 +38,27 @@
             $hover_canvas.attr('width', $image.width());
             $hover_canvas.attr('height', $image.height());
 
+            // set drawing style
+            var context = $hover_canvas[0].getContext('2d');
+            context.fillStyle = "rgba(" + hexR(options.fillColor) + ", "
+                + hexG(options.fillColor) + ", "
+                + hexB(options.fillColor) + ", "
+                + options.fillOpacity + ")";
+
+            context.strokeStyle = "rgba(" + hexR(options.strokeColor) + ", "
+                + hexG(options.strokeColor) + ", "
+                + hexB(options.strokeColor) + ", "
+                + options.strokeOpacity + ")";
+
+            context.lineWidth = options.strokeWidth;
+
             $image.before($hover_canvas);
             $image.before($event_img);
 
-            $map.find('area').hover(areaHover, clearCanvas);
+            $map.find('area').hover(areaHover, clearHoverCanvas);
             $map.find('area').click(areaClick);
 
             function areaHover(e){
-                //console.log('hover worked');
                 e.preventDefault();
                 drawArea($(this), $hover_canvas[0]);
             }
@@ -51,10 +68,9 @@
             }
 
             function drawArea($area, $canvas){
-                var context;
+                var context = $canvas.getContext('2d');
                 if($area.attr('shape').toLowerCase() == 'poly'){
                     var coords = $area.attr('coords').split(',');
-                    context = $canvas.getContext('2d');
                     context.beginPath();
                     context.moveTo(coords[coords.length-2], coords[coords.length-1]); //last point
                     for(var i = 0; i < coords.length; i+=2){
@@ -62,13 +78,34 @@
                     }
                     context.closePath();
                     context.stroke();
+                    context.fill();
                 }else{
                     console.log('drawing for non-polynomial areas not yet implemented');
                 }
             }
 
-            function clearCanvas(){
+            function clearHoverCanvas(){
+                clearCanvas($hover_canvas[0]);
             }
+
+            function clearCanvas($canvas){
+                var context = $canvas.getContext('2d');
+                context.clearRect(0, 0, $canvas.width, $canvas.height);
+            }
+
+            function hexR(h){
+                return parseInt((cutHex(h)).substring(0,2),16);
+            }
+            function hexG(h){
+                return parseInt((cutHex(h)).substring(2,4),16);
+            }
+            function hexB(h){
+                return parseInt((cutHex(h)).substring(4,6),16);
+            }
+            function cutHex(h){
+                return (h.charAt(0)=="#") ? h.substring(1,7):h;
+            }
+
         })
     };
 })(jQuery)
